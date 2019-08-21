8f28adc0d3edbcfbb2d779021360231b0b811056
Lars Trieloff <trieloff@adobe.com>
2019-08-21 16:06:21 +0000

    build(monitoring): migrate from helix-pingdom-status to helix-status

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@
 
 const openwhisk = require('openwhisk');
 const { logger: setupLogger } = require('@adobe/openwhisk-action-utils');
-const { wrap } = require('@adobe/helix-pingdom-status');
+const { wrap } = require('@adobe/helix-status');
 const resolvePreferred = require('./resolve-preferred');
 const { fetchers } = require('./fetchers');
 
