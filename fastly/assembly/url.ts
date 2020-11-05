9d57fccc16a3454a653148ff394e75f163752ad3
Lars Trieloff <trieloff@adobe.com>
2020-11-05 11:37:53 +0000

    feat(assembly): add helpers to url class

--- fastly/assembly/url.ts
+++ fastly/assembly/url.ts
@@ -1,8 +1,25 @@
 export class URL {
   protected url: string;
+  protected scheme: string;
+  protected host: string;
+  protected path: string;
+  protected qs: string;
 
   constructor(url:string) {
-    this.url = url;
+    const schemepairs = url.split("://");
+    this.scheme = schemepairs[0];
+
+    const fragments = schemepairs.pop().split("/");
+    this.host = fragments.shift();
+    this.url = "/" + fragments.join("/");
+    this.path = this.url;
+
+    if (this.url != null && this.url.indexOf("?") > 0) {
+      this.qs = this.url.substring(this.url.indexOf("?") + 1);
+      this.path = this.url.substring(0, this.url.indexOf("?"));
+    } else {
+      this.qs = "";
+    }
   }
 
   static decode(encoded:string): string {
@@ -16,20 +33,70 @@ export class URL {
     return encoded;
   }
 
-  querystring(): string {
-    if (this.url != null && this.url.indexOf("?") > 0) {
-      return this.url.substring(this.url.indexOf("?") + 1);
+  static encode(decoded: string): string {
+    let encoded = "";
+    for (let i = 0; decoded.charCodeAt(i) > -1; i++) {
+      const charcode = decoded.charCodeAt(i);
+      const character = decoded.charAt(i);
+      const pencoded = "%" + charcode.toString(16);
+      
+      if ((charcode >= 45 && charcode <= 46) || // -.
+          (charcode >= 48 && charcode <= 57) || // numbers
+          (charcode >= 65 && charcode <= 90) || // uppercase
+          (charcode >= 97 && charcode <= 122)   // lowercase
+      ) {
+        encoded += character;
+      } else {
+        encoded += pencoded;
+      }
+      
     }
-    return "";
+    return encoded;
+  }
+
+  get querystring(): string {
+    return this.qs;
   }
 
   queryparam(param: string, defaultValue: string): string {
-    const pairs = this.querystring().split("&");
+    const pairs = this.querystring.split("&");
     for (let i = 0; i < pairs.length; i++) {
       if (pairs[i].indexOf(param + "=")==0) {
         return URL.decode(pairs[i].substr(param.length + 1));
       }
     }
     return defaultValue;
+  }
+
+  append(fragment: string): URL {
+    let cleanpath = this.path;
+    let cleanfragment = fragment;
+    if (this.path.endsWith("/")) {
+      cleanpath = this.path.substring(0, this.path.length - 1);
+    }
+    if (cleanfragment.startsWith("/")) {
+      cleanfragment = cleanfragment.substring(1);
+    }
+
+    this.path = cleanpath + "/" + cleanfragment;
+
+    if (this.qs != "") {
+      this.url = this.path;
+    } else {
+      this.url = this.path + this.qs;
+    }
+    return this;
+  }
+
+  appendParam(key: string, value: string): URL {
+    this.qs = this.qs + "&" + key + "=" + URL.encode(value);
+
+    this.url = this.path + "?" + this.qs;
+
+    return this;
+  }
+
+  toString(): string {
+    return this.scheme + "://" + this.host + this.url;
   }
 }
--- test/assembly/url.spec.ts
+++ test/assembly/url.spec.ts
@@ -2,11 +2,12 @@ import { URL } from "../../fastly/assembly/url";
 
 const example1 = new URL('https://example.com/test.php?foo=bar&bar=nothing%20here');
 const example2 = new URL('https://example.com/test.php');
+const example3 = new URL('https://example.com');
 
 describe("url", () => {
   it("querystring", () => {
-    expect<string>(example1.querystring()).toBe("foo=bar&bar=nothing%20here");
-    expect<string>(example2.querystring()).toBe("");
+    expect<string>(example1.querystring).toBe("foo=bar&bar=nothing%20here");
+    expect<string>(example2.querystring).toBe("");
   });
 
   it("queryparam", () => {
@@ -14,5 +15,26 @@ describe("url", () => {
     expect<string>(example1.queryparam("bar", "")).toBe("nothing here");
 
     expect<string>(example2.queryparam("foo", "none")).toBe("none");
+  });
+
+  it("toString", () => {
+    expect<string>(example1.toString()).toBe("https://example.com/test.php?foo=bar&bar=nothing%20here");
+    expect<string>(example2.toString()).toBe("https://example.com/test.php");
+    expect<string>(example3.toString()).toBe("https://example.com/");
+  });
+
+  it("append", () => {
+    expect<string>(new URL("https://example.com").append("test.php").toString()).toBe("https://example.com/test.php");
+    expect<string>(new URL("https://example.com/").append("/test.php").toString()).toBe("https://example.com/test.php");
+    expect<string>(new URL("https://example.com/").append("/test/php").toString()).toBe("https://example.com/test/php");
+    expect<string>(new URL("https://example.com/").append("/test/").append("/php").toString()).toBe("https://example.com/test/php");
+  });
+
+  it("encode", () => {
+    expect<string>(URL.encode("Hello & Good-Bye.")).toBe("Hello%20%26%20Good-Bye.");
+  });
+
+  it("appendParam", () => {
+    expect<string>(example1.appendParam("greeting", "Hello & Good-Bye.").toString()).toBe("https://example.com/test.php?foo=bar&bar=nothing%20here&greeting=Hello%20%26%20Good-Bye.");
   });
 });
